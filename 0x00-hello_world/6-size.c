d5a13826b1f903f2a615b1500ba62342199cfec2
Kitemange <50658457+Kitemange@users.noreply.github.com>
2022-03-10 18:44:13 +0300

    sizeof-c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
     long long int e;
     float f;
 
-    printf("The size of char is: %lu byte(s)\n",sizeof(i));
-    printf("The size of int is: %lu byte(s)\n",sizeof(d));
-    printf("The size of long int is: %lu byte(s)\n",sizeof(c));
-    printf("The size of long long int is: %lu byte(s)\n",sizeof(e));
-    printf("The size of float is: %lu byte(s)\n",sizeof(f));
+    printf("The size of char is: %lu byte(s)\n", sizeof(i));
+    printf("The size of int is: %lu byte(s)\n", sizeof(d));
+    printf("The size of long int is: %lu byte(s)\n", sizeof(c));
+    printf("The size of long long int is: %lu byte(s)\n", sizeof(e));
+    printf("The size of float is: %lu byte(s)\n", sizeof(f));
 
     return 0;
 }
