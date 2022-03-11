fdd0198e7baea18cfb2b179a55e067445c669175
Kitemange <bryanmutheke18@gmail.com>
2022-03-12 00:35:35 +0300

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  * Always return(0)
  */
 
-int main (void)
+int main(void)
 {
 char ch = 'a';
 
-for(ch = 'a' ; ch <= 'z' ; ch++)
-      putchar(ch);
-   
-   return(0);
+for(ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+
+return (0);
 }
