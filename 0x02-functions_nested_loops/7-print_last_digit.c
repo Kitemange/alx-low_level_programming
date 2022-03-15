1062892595177a2edd42ad5945d68feb7d238723
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 19:05:40 +0300

    prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int n)
 
 {
 int last_digit;
-while (/* condition */last_digit >= 10)
+while (/* condition */n >= 10)
 {
     /* code */
-last_digit = last_digit /10 ;
-return (last_digit);
+last_digit = n /10 ;
+return (n);
 }
 
 
