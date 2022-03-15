572928817e36902a1fe42bf8614d4bab85e571fc
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 19:51:01 +0300

    prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * times_table - computes the absolute value of an integer
+ *
+ * Description: computes integer absolute value integer
+ * Return: Always(0)
+ */
+void times_table(void)
+{
+int e,g,d;
+for ( e = 0; e <= 9; e++)
+{
+    /* code */
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+
+for (g = 0; g <= 9; g++)
+{
+    /* code */
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+}
+
+}
