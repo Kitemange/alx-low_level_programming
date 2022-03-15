32299e8073b08e2116edd8af7934c17339a0ff37
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 16:46:56 +0300

    checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character.
+ * 
+ *return: always success. 
+ */
+
+int _islower(int c)
+if (/* condition */ cha)
+{
+    /* code */
+    return (1);
+}else
+{
+    /* code */
+    return (0);
+}
+
+
