5a6c8cced3e438576d24512cc47297ef2dec9598
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 18:46:48 +0300

    absolute value integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * int _abs - computes the absolute value of an integer
  * @n: integer to be tested
  *
- * Description: computes integer absolute value of an
+ * Description: computes integer absolute value integer
  * Return: Always(0)
  */
 int _abs(int n)
