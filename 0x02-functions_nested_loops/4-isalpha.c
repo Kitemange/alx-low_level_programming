9f562a6931277031d2bfa4fab5bb5f81f290acd2
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 17:42:00 +0300

    isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,18 +9,18 @@
 
 int _isalpha(int c)
 {
-for (c >= 'a'; c <= 'z'; c++)
+if(/* condition */c >= 'a' && c <= 'z')
 {
-if (/* condition */c >= 'A' && c >= 'Z')
+    /* code */
+return (1);
+}
+else if(/* condition */c >= 'A' && c <= 'Z')
 {
     /* code */
 return (1);
 }
 else
 {
-    /* code */
 return (0);
-}
-putchar(1);
 }
 }
