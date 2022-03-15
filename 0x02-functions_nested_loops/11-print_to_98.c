90f2dd9c3c4b1630e1c3ef5f31fac226c822ef78
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 20:39:50 +0300

    prints all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the lest digit
+ * @n: first parameter
+ *
+ * Description: prints the last digit
+ * Return: Always (0).
+ */
+
+void print_to_98(int n)
+{
+    n = 0;
+    while (/* condition */n <= 98)
+    {
+        /* code */
+        _putchar(n);
+        _putchar(',');
+        _putchar(' ');
+    }
+
+}
