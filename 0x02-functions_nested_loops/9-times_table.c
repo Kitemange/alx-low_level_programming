a003062e8d7fc8dfab7d4969061492cecb18ff12
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 20:20:35 +0300

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,39 @@
-#include<stdio.h>
 #include "main.h"
 /**
- * times_table - computes the absolute value of an integer
- *
- * Description: computes integer absolute value integer
- * Return: Always(0)
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void times_table(void)
 {
-int e, g, d;
-for (e = 0; e <= 9; e++)
-{
-    /* code */
-_putchar('0');
-_putchar(',');
-_putchar(' ');
+	int rone, cone, d;
 
-for (g = 0; g <= 9; g++)
-{
-    /* code */
-d = (e * g);
-if (/* condition */(d / 10) > 0)
-{
-    /* code */
-_putchar(d / 10);
-}
-else
-{
-    /* code */
-_putchar(' ');
-}
-_putchar((d % 10) + '0');
+	for (rone = 0; rone <= 9; rone++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
-if (g < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
 }
