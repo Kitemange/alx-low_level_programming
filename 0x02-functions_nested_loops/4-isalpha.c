408e1065994f9f2904ea1cdec505448678835a13
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 17:16:26 +0300

    checks if_is alpha_character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * @brief 
+ *
+ * 
+ */
+
+int _isalpha(int c)
+{
+if (/* condition */c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+{
+    /* code */
+return (1);
+}
+else
+{
+return (0);
+}
+}
