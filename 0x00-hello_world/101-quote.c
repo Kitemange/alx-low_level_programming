11b79c541ee6fc15ba5f9934e90080c8d5d59028
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 13:16:38 +0300

    push

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    int fd;
+    char buffer[80];
+    char msg[80] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+}
