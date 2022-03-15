3d8848b6784f6282f57228fe60d2bceb4d9d9210
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 19:40:35 +0300

    jack_bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,6 @@
 #include "main.h"
 /**
  * jack_bauer - computes the absolute value of an integer
- * @n: integer to be tested
  *
  * Description: computes integer absolute value integer
  * Return: Always(0)
@@ -13,16 +12,16 @@ int i, g;
 
 	for (i = 0; i < 24; i++)
 	{
-        for (g = 0; g < 60; g++)
-        {
-            /* code */
-            _putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((g / 10) + '0');
-			_putchar((g % 10) + '0');
-			_putchar('\n');
-        }
-    }
+for (g = 0; g < 60; g++)
+{
+    /* code */
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((g / 10) + '0');
+_putchar((g % 10) + '0');
+_putchar('\n');
+}
+}
         
 }
