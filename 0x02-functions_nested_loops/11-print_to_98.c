85eb32ed437cd2b2538f4451b1a8d68da665da37
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 21:06:10 +0300

    prints all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,90 +1,40 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - prints numbers to 98
- * @n: first parameter
- *
- * Description: prints all num to 98
- * Return: Always (0).
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
  */
-
 void print_to_98(int n)
 {
-int end;
-
-end = 98;
-
-if (n < end)
-{
-for (; n <= end; n++)
-{
-if (n == 98)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-break;
-}
-else
-{
-if (n >= -9 && n <= 9)
-{
-if (n < 0)
-{
-_putchar('-');
-_putchar((n * -1) + '0');
-_putchar(',');
-_putchar(' ');
-}
-else if (n >= 0)
-{
-_putchar(n + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-else if (n >= 100)
-{
-_putchar((n / 100) + '0');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
-else
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-}
-_putchar('\n');
-}
-else if (n == 0)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar('\n');
-}
-else
-{
-for (; n >= end; n--)
-{
-if (n == 98)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-break;
-}
-else
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
-return;
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar("%d", n);
+			if (n != 98)
+			{
+				_putchar(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar("%d", n);
+			if (n != 98)
+			{
+				_putchar(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		_putchar("98");
+	}
+	_putchar("\n");
 }
