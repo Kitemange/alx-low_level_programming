05da018c5cfc04260bfc5eac32da787c3d790416
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 20:23:02 +0300

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,16 @@
  */
 void times_table(void)
 {
-	int rone, cone, d;
+	int r, c, d;
 
-	for (rone = 0; rone <= 9; rone++)
+	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cone = 1; cone <= 9; cone++)
+		for (c = 1; c <= 9; c++)
 		{
-			d = (rone * cone);
+			d = (r * c);
 			if ((d / 10) > 0)
 			{
 				_putchar((d / 10) + '0');
@@ -27,7 +27,7 @@ void times_table(void)
 			}
 			_putchar((d % 10) + '0');
 
-			if (cone < 9)
+			if (c < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
