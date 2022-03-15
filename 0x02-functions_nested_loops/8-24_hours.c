6b323749028960bab35c34fea12ce7d4c70dccdc
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 19:29:53 +0300

    prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to be tested
+ *
+ * Description: computes integer absolute value integer
+ * Return: Always(0)
+ */
+void jack_bauer(void)
+{
+int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+        for (size_t g = 0; g < 60; g++)
+        {
+            /* code */
+        _putchar((i / 10) + '0');
+        }
+    }
+        
+}
