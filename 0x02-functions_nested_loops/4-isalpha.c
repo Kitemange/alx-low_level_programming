4181f39ad5f3bb03f3aedff07dabf19fd7e04b7a
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 17:24:10 +0300

    -salpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,26 +1,21 @@
+#include<stdio.h>
 #include "main.h"
 
 /**
- * test_isalpha - Test the _isalpha function
+ * _isalpha - looks for alphabetic characters.
  *
- * @n: Number to pass to _isalpha function
+ * return: 1,0.
  */
+
 int _isalpha(int c)
 {
-	int r;
-
-	r = _isalpha(c);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+if (/* condition */c >= 'a' && c <= 'z')
 {
-	test_isalpha(';');
-	return (0);
+    /* code */
+return (1);
+}
+else
+{
+return (0);
+}
 }
