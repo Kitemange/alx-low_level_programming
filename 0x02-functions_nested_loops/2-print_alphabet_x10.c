35a2cd1bb3c1a1275ed81b193c87f4d550dc459b
Kitemange <bryanmutheke18@gmail.com>
2022-03-15 16:29:59 +0300

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,26 +2,24 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - alphabet in lowercase, followed by new line.
+ * print_alphabet - alphabet in lowercase, followed by new line.
  *
  * Return: no return value
  */
-void print_alphabet_x10(void);
-int main(void)
-{
-    print_alphabet_x10();
-	return (0);
-}
 
 void print_alphabet_x10(void)
 {
+int count = 0;
 char al = 'a';
 
+for (count = 0; count < 10; count++)
+{
+    /* code */
 for (al = 'a'; al <= 'z'; al++)
 {
     /* code */
 putchar(al);
 }
 putchar('\n');
-
+}
 }
