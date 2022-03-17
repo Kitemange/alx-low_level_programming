60af8ae0ebc5ce121bbc9e28e7695532a35026ea
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 20:04:19 +0300

    prints digits 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * @c: ucheck_digit.
+ * _isdigit -prints digits 0-9.
+ * 
+ * return - 1,0.
+ * 
+ */
+
+int _isdigit(int c)
+{
+c = 0;
+while (c <= 9)
+{
+    /* code */
+printf("1\n",c);
+}
+return (0);
+}
