f080d73c883d0ec55c15a7e9d8216348f2c872f7
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 23:59:13 +0300

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints nultplies of 3 or 5
+ *
+ * Description: multiples between 0 and 100
+ * Return: Always(0)
+ */
+
+int main(void)
+{
+    int i;
+    for (i = 0; i <= 100; i++)
+    {
+        /* code */ 
+        if (/* condition */i % 3 == 0)
+        {
+            /* code */
+            printf("Fizz ");
+        }
+        else if (/* condition */i % 5 == 0)
+        {
+            /* code */
+            printf("Buzz ");
+        }
+        else if (/* condition */i % 15 == 0)
+        {
+            /* code */
+            printf("FizzBuzz ");
+        }
+        else
+        {
+            /* code */
+            printf("%i ",i);
+        }
+    }        
+        printf('\n');
+        return (0);
+    }
+    
