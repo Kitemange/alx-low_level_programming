80365426f32a8a19a9140894ae2ef374f4591e9b
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 20:31:08 +0300

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * @c: check_digit.
+ * _isdigit -prints digits 0-9.
+ * 
+ * return - 1,0.
+ * 
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (/* condition */i <= 9)
+{
+    /* code */
+_putchar('i');
+}
+_putchar('\n');
+}
