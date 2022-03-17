86e1eb8925e5606354f389a741d8548159866843
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 19:40:56 +0300

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+int _isupper(int c)
+{
+    for ( c = 'A'; c < 'Z'; c++)
+    {
+        /* code */
+        return (1);
+    }
+    return (0);
+    
+    
+}
