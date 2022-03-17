202678af916e7448fe736c71c5621f4e311236f0
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 20:20:42 +0300

    multiply_two_integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * @a: integer 1.
+ * @b: integer 2.
+ * _isdigit -prints digits 0-9.
+ * 
+ * return - Always 0.
+ * 
+ */
+
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+
+return (result);
+
+}
