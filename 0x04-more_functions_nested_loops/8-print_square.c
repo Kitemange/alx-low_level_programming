73d2aa42adb520b55d1be1af639c35583b105a1b
Kitemange <bryanmutheke18@gmail.com>
2022-03-17 23:46:57 +0300

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,21 +10,21 @@
 
 void print_square(int size)
 {
-	int i, j;
+int i, j;
 
-	if (/* condition */size <= 0)
-    {
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+if (/* condition */size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
