d7d0f8d5abbc12a29463e61985d07890c6b0a913
Kitemange <bryanmutheke18@gmail.com>
2022-03-18 16:10:31 +0300

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,11 +17,11 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(i / 14 + '0');
 			}
 			else
 			{
-				_putchar(i % 10 + '0');
+				_putchar(i % 14 + '0');
 			}
 		}
 		_putchar('\n');
