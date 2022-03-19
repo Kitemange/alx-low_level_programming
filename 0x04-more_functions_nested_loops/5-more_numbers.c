53e788c1062ba836541322c8bbea48071bf1bcd8
Kitemange <bryanmutheke18@gmail.com>
2022-03-19 11:48:49 +0300

    doo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 
 	for (n = 0; n <= 10; n++)
 	{
-		for (i = 0; i <= 10; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-				_putchar(i % 10 + '0');
+				_putchar(i / 10 + '1');
 			}
 			else
 			{
