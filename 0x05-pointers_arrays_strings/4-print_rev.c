be2ca53a061b29f61f8a19e4f4d888dc95352c04
Kitemange <bryanmutheke18@gmail.com>
2022-03-21 20:29:37 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * print_rev - reverse string
+ * @s: string in reverse
+ *
+ * Description: prints a string in reverse
+ * Return: Alwalys (0)
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
