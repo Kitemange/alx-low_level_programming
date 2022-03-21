3962b9ba7913da2f99188f2ec8881489a93cce27
Kitemange <bryanmutheke18@gmail.com>
2022-03-21 18:29:28 +0300

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include<string.h>
+
+/**
+ * _puts - prints a string
+ * @str: input parameter
+ *
+ * Description: prints a string
+ */
+
+void _puts(char *str)
+{
+while (/* condition */str != '\0')
+{
+    /* code */
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
