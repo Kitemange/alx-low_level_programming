fe94b9eaa78a5ab9507dac6daf5e979d8064c19e
Kitemange <bryanmutheke18@gmail.com>
2022-03-21 17:51:42 +0300

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,10 +3,10 @@
 #include<string.h>
 
 /**
- * @s:string length
+ * @s: string length
  * _strlen: returns the length of a string.
  *
- * description - function that returns the length of a string.
+ * Description - function that returns the length of a string.
  * return: always(0).
  */
 
@@ -14,10 +14,10 @@ int _strlen(char *s)
 {
 int l;
 
-    for (l = 0; s[l] != '\0'; l++)
-	{
-		continue;
-	}
+for (l = 0; s[l] != '\0'; l++)
+{
+continue;
+}
 
-	return (l);
+return (l);
 }
