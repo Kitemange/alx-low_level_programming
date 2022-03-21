52c96ee9f80b57528a06af63622619a7f8b1938d
Kitemange <bryanmutheke18@gmail.com>
2022-03-21 20:38:33 +0300

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,5 +12,23 @@
 
 void print_rev(char *s)
 {
-	strrev(s);
+	int l, i, *s, *e, t;
+
+	l = str_len(s);
+	s = s;
+	e = s;
+
+	for (i = 0; i < l; i++)
+	e++;
+
+	for (i = 0; i < l/2; i++)
+	{
+		/* code */
+		t = *e;
+		*e = *s;
+		*s = t;
+
+		s++;
+		e--;
+	}
 }
