c44484949afedfeb0f56fbb535d47ddcc8db683b
Kitemange <bryanmutheke18@gmail.com>
2022-03-22 13:08:51 +0300

    rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 
 /**
@@ -9,21 +8,22 @@
 
 void rev_string(char *s)
 {
-	int length, i, j;
+	int length, j, i;
 	char v1;
 
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
-		j = length - 1;
-		i = 0;
-		if (j > i)
-		{
-			/* code */
-			v1 = s[j];
-			s[j] = s[i];
-			s[i] = v1;
-			j--;
-			i++;
-		}
+
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		s[i] = s[j];
+		s[j] = v1;
+		j--;
+		i++;
+	}
 }
