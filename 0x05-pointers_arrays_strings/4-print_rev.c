08438f4cc19c8ff5875833060af67fae95b06baf
Kitemange <bryanmutheke18@gmail.com>
2022-03-22 12:04:03 +0300

    print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,9 @@ void print_rev(char *s)
 	int i = 0;
 	int length;
 
-	for (length = 0; s[length] != '\0'; length++)
-	{
-	}
+	if (length = 0 && s[length] != '\0')
+	
+		 length++;
 
 	for (i = length - 1; i >= 0; i--)
 	{
