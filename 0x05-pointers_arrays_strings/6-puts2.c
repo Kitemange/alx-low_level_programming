089dc0480891536333303f9272b33c52117fb8f2
Kitemange <bryanmutheke18@gmail.com>
2022-03-22 13:42:31 +0300

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,17 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
+		if (/* condition */ i % 2 == 0)
 		{
+			/* code */
 			_putchar(str[i]);
 		}
-		i++;
+		
 	}
+	
 	_putchar('\n');
 }
