9ad8e2dbc3ba29f2c3cca32f88b3f4b392eb02f4
Kitemange <bryanmutheke18@gmail.com>
2022-03-25 00:00:43 +0300

    hot_fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *string_toupper - changes all lowercase letters of a string to uppercase.
+ *@s: string to modify.
+ *
+ *Return: s modified.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	do
+	{
+		/* code */
+		i++;
+		if (/* condition */s[i] > 'a' && s[i] < 'z')
+		{
+			/* code */s[i] -= 32;
+		}
+		
+	} while (/* condition */s[i] != '\0');
+	return (s);
+}
