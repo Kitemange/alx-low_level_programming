611e13823bbce1c78958c975d53a93e6ba1c4c84
Kitemange <bryanmutheke18@gmail.com>
2022-03-24 23:23:37 +0300

    compare_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 
-	if (s1[i] != '\0' && s2[i] != '\0' && j == 0)
+	for (; s1[i] != '\0' && s2[i] != '\0' && j == 0; i++)
 	{
 		/* code */
-		j = s1[i] - s2[i];
-		i++;
-		
+		j = s1[i] - s2[i];		
 	}
 	return (j);
 }
