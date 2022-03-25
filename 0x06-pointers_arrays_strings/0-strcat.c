c62894aa473a5674c3e3eb01095915523846e547
Kitemange <bryanmutheke18@gmail.com>
2022-03-25 15:01:29 +0300

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,22 @@
  */
 
 char *_strcat(char *dest, char *src)
- {
-	 int i, l = 0;
-
-	 do
-	 {
-		 /* code */
-		 l++;
-	 } while (/* condition */dest[l] !=  '\0');
-	 for (i = 0; src[i] != '\0'; i++)
-	 {
-		 /* code */
-		 dest[l] = src[i];
-		 l++;
-	 }
-	 dest[l] ='\0';
+{
+	int i, l = 0;
+	
+	do
+	{
+		/* code */
+		l++;
+	}
+	while (/* condition */dest[l] !=  '\0');
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		/* code */
+		dest[l] = src[i];
+		l++;
+	}
+	dest[l] ='\0';
 
 	return (dest);
 }
