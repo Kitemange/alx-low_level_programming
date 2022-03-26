01a36201828da3bb8c16d672e11f197492506e2c
Kitemange <bryanmutheke18@gmail.com>
2022-03-26 12:26:05 +0300

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,21 +14,21 @@ char *cap_string(char *s)
 	while (/* condition */s[i] != '\0')
 	{
 		/* code */
-		if (/* condition */s[i] == ',' || s[i] == ';' || s[i] == '.' 
+		if (/* condition */s[i] == ',' || s[i] == ';' || s[i] == '.'
 						|| s[i] == '!' || s[i] == '?' || s[i] == '"'
 						|| s[i] == '(' || s[i] == ')' || s[i] == '{'
 						|| s[i] == '}' || s[i] == ' ' || s[i] == '\t'
 						|| s[i] == '\n')
 		{
 			/* code */
-			if (/* condition */s[i + 1] >= 'a' && s[i + 1] <='z')
+			if (/* condition */s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 			/* code */
 			s[i + 1] -= 32;
 			}
 		}
 	i++;
-		
+
 	}
 
 	return (s);
