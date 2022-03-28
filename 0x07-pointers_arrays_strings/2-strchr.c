ebef00b192dd42717f1a844c15cc375236ebc7e2
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 16:56:59 +0300

    execute

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memset - fill memory
+ * @s: string1
+ * @c: input size
+ *
+ * Description: locates a character in a string
+ * Return: returns the memory area.
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	
+	return (0);
+}
