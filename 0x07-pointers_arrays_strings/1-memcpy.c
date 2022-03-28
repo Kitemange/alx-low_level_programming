5cc25a0a2e4a4ee27933a3771a1b52d74d123bd3
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 14:24:52 +0300

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -16,11 +16,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	char *csrc = (char *)src;
 	char *cdest = (char *)dest;
 	unsigned int j;
-
 	for (j = 0; j < n; j++)
-		{
-			/* code */
-			cdest[j] = csrc[j];
-		}
-	return (cdest);
+	{
+		/* code */
+		cdest[j] = csrc[j];
+	}
+return (cdest);
 }
