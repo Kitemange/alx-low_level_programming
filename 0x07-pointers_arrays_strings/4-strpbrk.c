904853bfc00c875468e3b9848c51c3e2afce59ea
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 16:56:37 +0300

    execute

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - search a string
+ * @s: input string
+ * @accept: accepted string
+ *
+ * Description: searches a string for any of a set of bytes
+ * Return: resturns accepted string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if (/* condition */s == NULL || accept == NULL)
+	{
+		/* code */
+		return (NULL);
+	}
+	while (/* condition */*s)
+	{
+		/* code */
+		if (/* condition */)
+		{
+			/* code */
+		}
+		
+	}
+	
+	
+}
