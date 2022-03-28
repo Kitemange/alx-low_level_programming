654d8e0cfe0b80628035abe3e064e3933528dcbb
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 19:16:42 +0300

    save

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: input chars
+ *
+ * Description: print_chessboard
+ * Returns: always 0
+ */
+
+void print_chessboard(char (*a)[8])
