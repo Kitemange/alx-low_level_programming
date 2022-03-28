fe389a1e7685a858e472ddd617a2f1b81e714500
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 14:02:18 +0300

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _memset - fill memory
+ * @dest: string1
+ * @src: string2
+ * @n: input size
+ *
+ * Description: function that copies memory area
+ * Return: returns the memory area.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+	unsigned int j;
+	
+	for (j = 0; j < n; j++)
+		{
+			/* code */
+			cdest[j] = csrc[j];
+		}
+	return (0);
+}
