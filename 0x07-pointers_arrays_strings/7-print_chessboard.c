e525f7b32fadbec81cc97e353bfc23f73b745b84
Kitemange <bryanmutheke18@gmail.com>
2022-03-28 23:10:21 +0300

    execute

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_chessboard - print chessboard
@@ -17,17 +16,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (j == 7)
-			{
-				/* code */
-				_putchar(a[i][j]);
-				_putchar('\n');
-			}
-			else
-			{
-				/* code */
-				_putchar(a[i][j]);
-			}
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
