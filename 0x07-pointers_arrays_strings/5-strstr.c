c911180bf3358813d49c246eecfa6d48d11566c2
Kitemange <bryanmutheke18@gmail.com>
2022-03-29 18:26:02 +0300

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,28 +15,28 @@ char *_strstr(char *haystack, char *needle)
 {
 	   int i;
 
-	   if (!needle)
-	   {
-		   return (haystack);
-	   }
-	   while (*haystack != '\0')
-	   {
-		   i = 0;
+if (!needle)
+{
+return (haystack);
+}
+while (*haystack != '\0')
+{
+i = 0;
 
-		   while (*haystack != *needle && *haystack != '\0')
-		   {
-			   haystack++;
+while (*haystack != *needle && *haystack != '\0')
+{
+haystack++;
 
-			while (haystack[i] == needle[i] && haystack[i] != '\0')
-			{
-				i++;
-			}
-			if (needle[i] == '\0')
-			{
-				return (haystack);
-			}
-		}
-		haystack++;
-	}
-	return  (0);
+while (haystack[i] == needle[i] && haystack[i] != '\0')
+{
+i++;
+}
+if (needle[i] == '\0')
+{
+return (haystack);
+}
+}
+haystack++;
+}
+return  (0);
 }
