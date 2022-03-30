3ee0dfb3f0a81b1ce4cea8fc914a3659e3afba63
Kitemange <bryanmutheke18@gmail.com>
2022-03-31 01:04:34 +0300

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -9,24 +7,3 @@
  * Description: finding the length of a string
  * Return: returns string's length
  */
-
-
- 
-int is_palindrome(char *s)
-{
-    int i,c=0,n;
-    
-    n=strlen(s);
-	for(i=0;i<n/2;i++)  
-    {
-    	if(s[i]==s[n-i-1])
-    	c++;
- 
- 	}
- 	if(c==i)
-        return 1;
-    else
-        return 0;
- 
- 	
-}
