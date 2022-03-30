0213a100af5e14ed1829c7d724ededf9b096e40d
Kitemange <bryanmutheke18@gmail.com>
2022-03-30 22:56:44 +0300

    execute

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion _prints a string followed by a new line
+ * @s - string
+ *
+ * Return: 1 on success
+ * on error, return -1.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (/* condition */*s ==  '\0')
+	{
+		/* code */
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	
+}
