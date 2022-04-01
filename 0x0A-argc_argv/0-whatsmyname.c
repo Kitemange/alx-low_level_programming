dc4aca9ffe712563c4719a6d83d6e4891dd202b7
Kitemange <bryanmutheke18@gmail.com>
2022-04-01 13:29:15 +0300

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - prints file name
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: a program that prints its name
+ * Return: Always(0) Success
+ */
+
 int main(int argc, char *argv[])
 {
 	/* code */
