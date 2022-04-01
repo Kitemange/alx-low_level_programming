6c0ad72ffe43e1071f0ed73e9c2e8fd4d8a988e9
Kitemange <bryanmutheke18@gmail.com>
2022-04-01 15:11:22 +0300

    all_arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: prints number of arguments
+ * Return: 0 for code success
+ */
+
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+	/* code */
+    for (; i < argc; i++)
+    {
+        /* code */
+        printf("%s\n",argv[i]);
+    }
+
+	return 0;
+}
