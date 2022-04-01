de473bc65f8faef671a52dcce27d61e132cd786a
Kitemange <bryanmutheke18@gmail.com>
2022-04-01 18:06:55 +0300

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,22 +8,23 @@
  * Return: 0 for success
  */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	int i, k = 0;
-	for ( i = 0; i < argc; i++)
+	int i, k;
+
+	k = 0;
+
+	for (i = 0; i < argc; i++)
 	{
 		if (i > 0)
 		{
-			/* code */
-			if (/* condition */(atoi(argv[i]) == 0))
+			if (atoi(argv[i]) == 0)
 			{
-				return(printf("error\n"), 1);
+				return (printf("Error\n"), 1);
 			}
-		k +=atoi(argv[i]);
+			k += atoi(argv[i]);
 		}
 	}
 	printf("%d\n", k);
-
 	return (0);
 }
