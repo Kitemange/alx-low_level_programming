b033579d048a382d95e5832fd99997d8893de779
Kitemange <bryanmutheke18@gmail.com>
2022-04-04 16:59:46 +0300

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 
 	duplicate = malloc(sizeof(char) * (l + 1));
 
-	if (str == NULL || duplicate == NULL)
+	if (str == 0 || duplicate == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
