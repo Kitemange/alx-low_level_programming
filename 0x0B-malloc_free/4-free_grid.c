a5f9de491b40e5ef437bfdf4ce39dc2eddc4d696
Kitemange <bryanmutheke18@gmail.com>
2022-04-05 00:10:57 +0300

    free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,13 +13,13 @@
 
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < height; i++)
-    {
-        /* code */
-        free(grid[i]);
-    }
-    
-    free(grid);
+	for (i = 0; i < height; i++)
+	{
+		/* code */
+		free(grid[i]);
+	}
+
+	free(grid);
 }
