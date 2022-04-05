c37717bdfc904fb81cc119760cdf861ea06c38f9
Kitemange <bryanmutheke18@gmail.com>
2022-04-05 23:23:51 +0300

    argstost

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
- * argrostr - concatenates all the arguments
- * @ac: input parameter.
- * @av: input parameter.
- * 
- * return: nothing. 
+ * argstostr - concatenates all the arguments
+ * @ac: input params
+ * @av: input params
+ *
+ * Return: nothing.
  */
 
 char *argstostr(int ac, char **av)
 {
-    int i, len = 1;
-    char *str;
+	int x, j, v = 0;
+	int len = 1;
+	char *str;
 
-    if (ac == 0 || av == 0)
-        return (NULL);
-    
-    for (i = 0; i < ac; i++)
-    {
-        len += strlen(av[i]);
-    }
-    str = malloc(sizeof(char)*len);
-    str[0] = '\0';
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 1; i < ac; i++)
-    {
-        strcat(str, av[i]);
-    }
-    if (str[i] != '\0')
-    {
-        return (str[i]);
-    }
-    else
-    {
-        return (NULL);
-    }
-    free(str);
+	for (x = 0; x < ac; x++)
+	{
+		for (j = 0; av[x][j] != '\0'; j++)
+		{
+			len += 1;
+		}
+		len += 1;
+	}
+	str = malloc(sizeof(char) * len);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (j = 0; av[x][j] != '\0'; j++)
+		{
+			str[v] = av[x][j];
+			v++;
+		}
+		str[v] = '\n';
+		v++;
+	}
+	str[v] = '\0';
+
+	if (str != NULL)
+	{
+		return (str);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
