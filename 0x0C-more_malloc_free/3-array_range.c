0938dbb8d95fb2a970d6667a370a32ce84fb819e
Kitemange <bryanmutheke18@gmail.com>
2022-04-07 23:58:22 +0300

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,34 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 /**
- * array_range - creates an array of integers
- * @min: first intput parameter
- * @max: second input parameter
+ * array_range -  creates an array of integers
+ * The array created should contain all the values
+ * from min (included) to max (included), ordered from min to max
+ * @min: minimal value
+ * @max: maximum value
  *
- * Return: Always(0) Success
+ * Return: Pointer to allocated memory of s1 + nbytes of s2
  */
-
 int *array_range(int min, int max)
 {
-	unsigned int len, i = 0;
 	int *p;
+	int size, i, j;
+
+	if (min > max)
+		return (0);
 
 	if (max > min)
-		len = min - max + 1;
-
-	if (min > max || !(p = malloc(sizeof(int) * len)))
-		return (NULL);
-	
+		size = max - min + 1;
 	else if (max == min)
-		len = 2;
-	
-	for (; min <= max; i++)
-		p[i] = min++;
+		size = 2;
+
+	p = malloc(sizeof(int) * size);
+	if (p == 0)
+		return (0);
+
+	for (i = 0, j = min; j <= max; i++, j++)
+		p[i] = j;
 	if (max == min)
 		p[i] = max;
-
 	return (p);
 }
