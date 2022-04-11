acb05194be2c99c7d17c0fe637e6cb9a71bbd4af
Kitemange <bryanmutheke18@gmail.com>
2022-04-11 21:59:52 +0300

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,22 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * struct dog - dog
+ *   * @name: name of the dog
+ *    * @age: how old the dog is
+ *     * @owner: who owns the dog
+ *      */
+typedef struct dog
 {
     /* data */
     char *name;
-    float age;
-    char *owner;
-}dog;
+        float age;
+            char *owner;
+}dog_t;
 
 
 #endif
