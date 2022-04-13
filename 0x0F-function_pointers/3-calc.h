a25c375159768c68f8f50eb43752c72938e45f7a
Kitemange <bryanmutheke18@gmail.com>
2022-04-13 23:39:28 +0300

    simple operations

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,51 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ * @a:num1
+ * @b:num2
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
+/*to add two values
+* Return: a + b
+*/
+int op_add(int a, int b);
+
+/*
+* subtract two values
+* Return: a - b
+*/
+int op_sub(int a, int b);
+
+/*
+* to multiply two values
+* Return: a * b
+*/
+int op_mul(int a, int b);
+
+/*
+* to divide two values
+* Return: a / b
+*/
+int op_div(int a, int b);
+
+/*
+* to give module two values
+* Return: a % b
+*/
+int op_mod(int a, int b);
+
+#endif
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+    register int a, b;
+    int (*p)(int, int);
+
+    /*number of arguments is wrong,*/
+    if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+    p = get_op_func(argv[2]);
+    /*the operator is none of the ones provided*/
+    if (!p)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    /*atoi to convert arguments to int*/
+    a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", p(a, b));
+
+    return (1);
+}
