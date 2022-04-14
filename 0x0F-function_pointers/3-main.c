c60d2e37c6cfc0c4f577126b5b1a5b9a8bb044f5
Kitemange <bryanmutheke18@gmail.com>
2022-04-14 10:47:18 +0300

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,28 +11,28 @@
 
 int main(int argc, char *argv[])
 {
-    int a, b;
-    int (*p)(int, int);
+	int a, b;
+	int (*p)(int, int);
 
-    /*number of arguments is wrong,*/
-    if (argc != 4)
+	/*number of arguments is wrong,*/
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-    p = get_op_func(argv[2]);
-    /*the operator is none of the ones provided*/
-    if (!p)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	p = get_op_func(argv[2]);
+	/*the operator is none of the ones provided*/
+	if (!p)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    /*atoi to convert arguments to int*/
-    a = atoi(argv[1]);
+	/*atoi to convert arguments to int*/
+	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	printf("%i\n", p(a, b));
 
-    return (1);
+	return (1);
 }
