b352b32a9eb1b3b1bc996b0363555f20d8e9cd03
Kitemange <bryanmutheke18@gmail.com>
2022-04-14 10:43:15 +0300

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,28 +1,38 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - performs simple operations
+ * main - main function
  * @argc: argument count
- * @argv: argument vector
- * Return: 1 for success
+ * @argv: string of arguments in array
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	register int a, b;
-	int (*fptr)(int, int);
+    int a, b;
+    int (*p)(int, int);
 
-	if (argc != 4)
+    /*number of arguments is wrong,*/
+    if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+		exit(98);
 	}
-	fptr = get_op_func(argv[2]);
-	if (!fptr)
-	{
-		printf("Error\n"), exit(99);
-	}
-	a = atoi(argv[1]);
+
+    p = get_op_func(argv[2]);
+    /*the operator is none of the ones provided*/
+    if (!p)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    /*atoi to convert arguments to int*/
+    a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	printf("%i\n", fptr(a, b));
-	return (1);
+	printf("%i\n", p(a, b));
+
+    return (1);
 }
