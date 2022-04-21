6294fdeefea927f10cff4c71a72c69fc8c1c9f46
Kitemange <bryanmutheke18@gmail.com>
2022-04-21 19:27:27 +0300

    Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .text
-    global _start
+	global _start
 
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, 13
-    syscall
-    
-    mov rax, 60
-    mov rdi, 0
-    syscall
+start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 13
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
 
 section .data
-    msg db "Hello, Holberton",10
-    msglen: equ $ - msg
+	msg db "Hello, Holberton",10
+	msglen: equ $ - msg
