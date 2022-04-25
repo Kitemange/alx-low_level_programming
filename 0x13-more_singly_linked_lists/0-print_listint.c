0ccde5ca429a9df41c4a56349bc8bb5fbc0b9eab
Kitemange <bryanmutheke18@gmail.com>
2022-04-25 19:36:37 +0300

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+    register int start = 0;
+
+    while (h != NULL)
+    {
+        printf("%i\n", h -> n);
+
+        h = h -> next;
+        start++;
+    }
+    return (start);
+}
