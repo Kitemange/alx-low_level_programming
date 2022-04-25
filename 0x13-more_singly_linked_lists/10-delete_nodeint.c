832f4519ec26eb74bde3ca1ed073cd8a3d047348
Kitemange <bryanmutheke18@gmail.com>
2022-04-26 00:25:44 +0300

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,37 @@
 #include "lists.h"
-
-
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t
+ * @head: pointer to head pointer
+ * @index: index of the node that should be deleted. Index starts at 0
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-      /*temp is used to freeing the memory*/
-      listint_t *temp;
+	listint_t *tmp;
+	listint_t *removeNode;
+	unsigned int i;
 
-      /*key found on the head node.*/
-      /*move to head node to the next and free the head.*/
-      if((*head)->n == index)
-      {
-          temp = *head;    /*backup to free the memory*/
-          *head = (*head)->next;
-          free(temp);
-          return (1);
-      }
-      else
-      {
-          listint_t *current  = *head;
-          while(current->next != NULL)
-          {
-              /*if yes, we need to delete the current->next node*/
-              if(current->next->n == index)
-              {
-                  temp = current->next;
-                  /*node will be disconnected from the linked list.*/
-                  current->next = current->next->next;
-                  free(temp);
-                  break;
-              }
-              /*Otherwise, move the current node and proceed*/
-              else
-                  current = current->next;
-          }
-      }
-    return (1);
+	i = 0;
+	tmp = *head;
+	while (tmp)
+	{
+		if (index == 0)
+		{
+			removeNode = *head;
+			*head = removeNode->next;
+			free(removeNode);
+			return (1);
+		}
+		if (i == index - 1)
+		{
+			removeNode = tmp->next;
+			tmp->next = removeNode->next;
+			free(removeNode);
+			return (1);
+		}
+        /*Otherwise, move the current node and proceed*/
+		tmp = tmp->next;
+		i++;
+	}
+	return (-1);
 }
