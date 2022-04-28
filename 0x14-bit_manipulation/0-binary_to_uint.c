ecdee49ed753a7857541d8e55a866778d8e17ec4
Kitemange <bryanmutheke18@gmail.com>
2022-04-28 17:59:15 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,47 @@
 #include "main.h"
-#include <stdlib.h>
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to s
+ *
+ * Return: 0 on success
+ *
+ */
+int _strlen(const char *s)
+{
+	int count = 0;
 
+	if (s != '\0')
+	{
+		while (*(s + count) != '\0')
+			count++;
+	}
+	return (count);
+}
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointing to a string of 0 and 1 chars
  * Return: the converted number, or 0 if there is one or more chars in the
  *  string b that is not 0 or 1 or b is NULL
  */
-
-unsigned int binary_to_uint( const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
-	unsigned int i=0;
-	int c;
-	unsigned int len;
+	unsigned int number;
+	int i, len, pow_of_2;
 
-	len = strlen(b);
-
-	for (c = len-1; c >= 0; c--)
+	len = _strlen(b);
+	number = 0;
+	pow_of_2 = 1;
+	if (b == NULL)
+		return (0);
+	for (i = len - 1; i >= 0 ; i--)
 	{
-		if (b[c] != '0' && b[c] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-			if (b[c] == '1')
-				i += k;
-	k *= 2;
+		if (b[i] == '0')
+			number += 0;
+		if (b[i] == '1')
+			number += pow_of_2;
+		pow_of_2 *= 2;
 	}
-	return (i);
+	return (number);
 }
