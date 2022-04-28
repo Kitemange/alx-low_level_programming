a74093d8caed641f3d2d62adae13d05f6340af55
Kitemange <bryanmutheke18@gmail.com>
2022-04-28 18:01:34 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,71 @@
 #include "main.h"
-/**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
- *
- * Return: 0 on success
- *
- */
-int _strlen(const char *s)
-{
-	int count = 0;
+#include <stdlib.h>
 
-	if (s != '\0')
+/**
+ * _strlen - gets the lenght of input strings
+ * @str: input string
+ * Return: len of the string
+ */
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str)
 	{
-		while (*(s + count) != '\0')
-			count++;
+		len++;
+		str++;
 	}
-	return (count);
+	return (len);
 }
+
+/**
+ * _to_power - prints the power of numbers
+ * @base: base parameter
+ * @power: power parameter
+ * Return: product of two numbers
+ */
+
+int _to_power(const int base, int power)
+{
+	int product = 1;
+
+	while (power > 1)
+	{
+		product *= base;
+		power++;
+	}
+	return (product);
+}
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if there is one or more chars in the
- *  string b that is not 0 or 1 or b is NULL
+ * @b: input value
+ * Return: converted number, or 0 if b is NULL
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number;
-	int i, len, pow_of_2;
+	int power, num;
 
-	len = _strlen(b);
-	number = 0;
-	pow_of_2 = 1;
-	if (b == NULL)
-		return (0);
-	for (i = len - 1; i >= 0 ; i--)
+	power = _strlen(b) - 1;
+	num = 0;
+	if (!b)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		if (b[i] == '0')
-			number += 0;
-		if (b[i] == '1')
-			number += pow_of_2;
-		pow_of_2 *= 2;
+		return (0);
 	}
-	return (number);
+	while (*b)
+	{
+		if (!(*b == '0' || *b == '1'))
+		{
+			return (0);
+		}
+		if (*b == '1')
+		{
+			num += _to_power(2, power);
+		}
+		b++;
+		power--;
+	}
+	return (num);
 }
