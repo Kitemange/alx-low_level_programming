68a895a49794db2d25164be00ae26342019d3dc9
Kitemange <bryanmutheke18@gmail.com>
2022-04-28 14:54:48 +0300

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,6 +10,6 @@
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+		print_binary(n >> 1);/*right_shift by 1*/
+	_putchar((n & 1)/*AND bitwise*/ + '0');
 }
