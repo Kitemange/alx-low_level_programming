53d4189d229875718c96d7287c3795d78755ea7b
Kitemange <bryanmutheke18@gmail.com>
2022-04-28 18:24:17 +0300

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8 - 1)/*Error*/
 		return (-1);
-        /*Use bitwise OR to check value returned by the left shift*/
+		/*Use bitwise OR to check value returned by the left shift*/
 	*n |= (1 << index);
 	return (1);
 }
