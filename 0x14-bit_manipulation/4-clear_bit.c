8a6e01b80896726d650fecdd5902dec2459ff9e9
Kitemange <bryanmutheke18@gmail.com>
2022-04-28 18:30:27 +0300

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+		if (index > sizeof(n) * 8 - 1)/*Error*/
+		return (-1);
+		/*Use bitwise OR to check value returned by the left shift*/
+	*n &= ~(1 << index);
+	return (1);
+}
