b03283f8f6fe71eb5dcfaaa2cc32e1a0d6a5c5cb
Kitemange <bryanmutheke18@gmail.com>
2022-05-03 17:19:38 +0300

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fl, wr, t_len = 0;
+	int fl, wr, t_len = 0;
 
-    if (filename == NULL)
-        return (-1);
-    fl = open(filename, O_RDWR | O_CREAT | O_TRUNC/*truncates if file exists*/
-                , 0600/*permission*/);
-    if (fl == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	fl = open(filename, O_RDWR | O_CREAT | O_TRUNC/*truncates if file exists*/
+				, 0600/*permission*/);
+	if (fl == -1)
+		return (-1);
 
-    /* condition to loop into text_content */
-    if (text_content)
-    {
-        while (text_content[t_len])
-            t_len++;
-        wr = write(fl, text_content, t_len);
-        if (wr == -1)
-        {
-            return (-1);
-        }        
-    }
-    close(fl);
-    return (1);/*create new file on success*/  
+	/* condition to loop into text_content */
+	if (text_content)
+	{
+		while (text_content[t_len])
+			t_len++;
+		wr = write(fl, text_content, t_len);
+		if (wr == -1)
+		{
+			return (-1);
+		}
+	}
+	close(fl);
+	return (1);/*create new file on success*/
 }
