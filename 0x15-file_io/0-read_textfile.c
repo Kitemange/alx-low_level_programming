7f88f1c609ba8a476c61a2097748372554a71517
Kitemange <bryanmutheke18@gmail.com>
2022-05-03 14:27:10 +0300

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,35 @@
 #include "main.h"
 
+/**
+ * read_textfile - a function that reads a
+ * text file and prints it to the POSIX standard output
+ * @filename: of the file
+ * @letters: the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int op, rd, wr;
-    char *buffer = malloc(sizeof(letters));
+	register int op, rd, wr;
+	register char *buffer = malloc(sizeof(char)  * letters);
 
-    if (filename == 0)
-        return (0);
-    if (!(buffer))
-    {
-        free(buffer);
-        return (0);
-    }
-    op = open(filename, O_RDONLY);
-    rd = read(op, buffer, letters);
-    wr = write(STDOUT_FILENO, buffer, rd);
-        
-    if (op == -1 || rd == -1 || wr == -1 || !(wr == rd))
-        return (0);
-    free(buffer);
-    close(op);
-    
-    return (wr);
+	if (!(buffer))
+	{
+		free(buffer);
+		return (0);
+	}
+	if (!(filename))
+		return (0);
+	op = open(filename, O_RDONLY);
+	rd = read(_open, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, _read);
+
+	if (op == -1 || rd == -1 || wr == -1 || !(wr == rd))
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(op);
+	return (wr);
 }
