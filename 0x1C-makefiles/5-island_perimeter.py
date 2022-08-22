06bfb458019b2f00188c9b3f9b2f0ef404923f28
Kitemange <bryanmutheke18@gmail.com>
2022-08-22 11:28:04 +0300

    make file updates

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
+""" This module contains the island_perimeter function """
+
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid
+    """ A function that returns the perimeter of island described in grid """
 
-    Args:
-        grid(matrix): input grid
+    perimeter = 0
 
-    Description:
-        Traverse the land mass, and
-        for the lake's nearest neighbor
-        then return the total length
+    nrows = len(grid)
 
-    """
-    y = 0
-    x = 0
-    
-    for row in range(1, len(grid) - 1):
-            for col in range(1, len(grid[row]) - 1):
-                if grid[row][col] == 1:
-                    if grid[row][col - 1] == 0:
-                        y += 1
-                    if grid[row][col + 1] == 0:
-                        y += 1
-                    if grid[row - 1][col] == 0:
-                        x += 1
-                    if grid[row + 1][col] == 0:
-                        x += 1
-    return x + y
+    if grid != []:
+        ncolumns = len(grid[0])
+
+    for i in range(nrows):
+        for j in range(ncolumns):
+            if grid[i][j] == 1:
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) == nrows or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) == ncolumns or grid[i][j + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
